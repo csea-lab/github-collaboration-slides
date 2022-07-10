6e8a001d4a39003e5913e7bacd2842492495fad5
Mears-UFL <16980450+Mears-UFL@users.noreply.github.com>
2022-07-10 13:09:37 -0400

    added many links and new pics

---
+++ git-rebase-graph.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(DiagrammeR)
+
+node_df <-
+  create_node_df(
+    n = 11,
+    type = c("b", "b", "b", "b", "b", "c", "c", "c", "a", "a", "a"),
+    label = LETTERS[1:11],
+    style = "filled",
+    color = "aqua",
+    shape = c("circle", "circle","circle", "circle","circle", "circle","circle", "circle"),
+    value = c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0))
+
+node_df
+
+edge_df <-
+  create_edge_df(
+    from = c(2, 3, 4, 5, 6, 7, 8,9,10,11),
+    to = c(1, 2, 3, 4, 2, 6, 7, 3,9,10),
+    rel = "related")
+
+
+graph <- create_graph(nodes_df = node_df,edges_df=edge_df)
+
+render_graph(graph,layout = "nicely") #,output = "visNetwork")
+
+grViz(graph,engine="visNetwork")
